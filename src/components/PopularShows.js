3d0b650c827880b858cfb966ce4c3c9948843683
Charlie Parrott <cparrott5700@gmail.com>
2021-09-18 01:53:01 -0400

    Adding image for shows

--- src/components/PopularShows.js
+++ src/components/PopularShows.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Community from './img/community_tv_series-239934497-large.jpg';
+import Simpsons from './img/au_disneynews_thesimpsons_season31_incarticle_poster_8f0c2ae5.jpg';
+import Dexter from './img/dexter_tv_series-197972047-large.jpg'
 
 // import { Link } from "react-router-dom";
 
@@ -16,16 +19,13 @@ function PopularShows () {
             <div className="container-fluid text-center">
               <div className="options d-flex flex-md-row flex-wrap justify-content-center">
                 <div className="rect">
-                  <img src="" alt=""></img>
+                  <img src={Community} className="card-img-top embed-responsive-item" alt="Community"></img>
                 </div>
                 <div className="rect">
-                  <img src="" alt=""></img>
+                  <img src={Simpsons} className="card-img-top embed-responsive-item" alt="Simpsons"></img>
                 </div>
                 <div className="rect">
-                  <img src="" alt=""></img>
-                </div>
-                <div className="rect">
-                  <img src="" alt=""></img>
+                  <img src={Dexter} className="card-img-top embed-responsive-item" alt="Dexter"></img>
                 </div>
               </div>
             </div>
