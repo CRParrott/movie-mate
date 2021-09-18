8f6a84fd9bba4f0170f9a9356ed9a75b681eb74d
Charlie Parrott <cparrott5700@gmail.com>
2021-09-18 01:42:37 -0400

    Popular movies file not working

--- src/components/PopularMovies.js
+++ src/components/PopularMovies.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import trendingMovies from "./api/trendingMovie";
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+// import trendingMovies from "./api/trendingMovie";
 
-function PopularMovies () {
-  return (
-    trendingMovies()
-  )
-};
+// function PopularMovies () {
+//   return (
+//     trendingMovies()
+//   )
+// };
 
-export default PopularMovies;
+// export default PopularMovies;
 
