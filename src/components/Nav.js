08b0bc35fdcbaefbe64e9e2841736091e41c1f10
Charlie Parrott <cparrott5700@gmail.com>
2021-09-18 12:38:07 -0400

    Adding updated README

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
 import "../index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function Nav() {
--- src/components/PopularShows.js
+++ src/components/PopularShows.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 // import { Link } from "react-router-dom";
 
